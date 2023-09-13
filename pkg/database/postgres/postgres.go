d09b4d902be3dd3551d0b1f2894235212ac05b98
Sergey Cherepiuk <s.cherepiuk@gmail.com>
2023-09-13 16:06:42 +0200

    Postgres database connection

---
+++ pkg/database/postgres/postgres.go
@@ -0,0 +1,23 @@
+package postgres
+
+import (
+	"fmt"
+	"os"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/jmoiron/sqlx"
+)
+
+var db *sqlx.DB
+
+func init() {
+	dsn := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s",
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_HOST"),
+		os.Getenv("POSTGRES_PORT"),
+		os.Getenv("POSTGRES_DBNAME"),
+	)
+	db = sqlx.MustConnect("pgx", dsn)
+}
