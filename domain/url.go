ec4d03f5740ff439a8b3b2f23a9db5e32279b178
Sergey Cherepiuk <s.cherepiuk@gmail.com>
2023-09-13 16:20:03 +0200

    Domain layer services

--- domain/url.go
+++ domain/url.go
@@ -7,3 +7,11 @@ type Url struct {
 	Hash     hash.Hash32 `json:"hash" db:"hash"`
 	Origin   string      `json:"origin" db:"origin"`
 }
+
+type UrlService interface {
+	GetAll(username string) ([]Url, error)
+	GetOrigin(username, hash string) (string, error)
+	Create(username, origin string) (Url, error)
+	Update(username, hash, newOrigin string) (Url, error)
+	Delete(username, hash string) (Url, error)
+}
--- domain/user.go
+++ domain/user.go
@@ -4,3 +4,15 @@ type User struct {
 	Username string `json:"username" db:"username"`
 	Password string `json:"password" db:"password"`
 }
+
+type SessionManagerService interface {
+	Login(username, password string) error
+	Logout(username string) error
+}
+
+type AccountManagerService interface {
+	Get(username string) (User, error)
+	Create(user User) (User, error)
+	Update(username string, updates map[string]any) (User, error)
+	Delete(user User) (User, error)
+}
