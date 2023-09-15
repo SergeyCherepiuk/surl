10debeb2882918852ddcd6d76cd18d0140aa1cdf
Sergey Cherepiuk <s.cherepiuk@gmail.com>
2023-09-15 09:59:04 +0200

    Start encrypting passwords and changed status codes for render methods

--- pkg/http/handlers/user.go
+++ pkg/http/handlers/user.go
@@ -8,6 +8,7 @@ import (
 	"github.com/SergeyCherepiuk/surl/domain"
 	"github.com/SergeyCherepiuk/surl/pkg/http/validation"
 	"github.com/labstack/echo/v4"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type UserHandler struct {
@@ -22,15 +23,17 @@ func (h UserHandler) SingUp(c echo.Context) error {
 	}
 
 	if err := validation.ValidateUserSignUp(user); err != nil {
-		data := struct {
-			User         domain.User
-			ErrorMessage string
-		}{user, err.Error()}
-		return c.Render(http.StatusBadRequest, "signup-error", data)
+		return c.Render(http.StatusOK, "component/error", err.Error())
 	}
 
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Username), 10)
+	if err != nil {
+		return c.Render(http.StatusOK, "component/error", "Failed to encrypt your password. Please try again.")
+	}
+	user.Password = string(hashedPassword)
+
 	if err := h.AccountManagerService.Create(context.Background(), user); err != nil {
-		return c.Render(http.StatusInternalServerError, "signup-error", "Something went wrong. Please try again.")
+		return c.Render(http.StatusOK, "component/error", "Failed to save your account to the database. Please try again.")
 	}
 
 	ttl := 7 * 24 * time.Hour
@@ -41,10 +44,10 @@ func (h UserHandler) SingUp(c echo.Context) error {
 	}
 
 	c.SetCookie(&http.Cookie{
-		Name: "session_id",
-		Value: id.String(),
+		Name:     "session_id",
+		Value:    id.String(),
 		HttpOnly: true,
-		Expires: time.Now().Add(ttl),
+		Expires:  time.Now().Add(ttl),
 	})
 	c.Response().Header().Set("HX-Redirect", "/")
 	return c.NoContent(http.StatusSeeOther)
