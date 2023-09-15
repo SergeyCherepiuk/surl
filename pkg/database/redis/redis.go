3f2dab6c47ceae78b447de4a7f44185b38853513
Sergey Cherepiuk <s.cherepiuk@gmail.com>
2023-09-15 09:56:44 +0200

    Fixed Redis connection

--- pkg/database/redis/redis.go
+++ pkg/database/redis/redis.go
@@ -13,7 +13,7 @@ var db *redis.Client
 
 func MustConnect() {
 	addr := fmt.Sprintf("%s:%s", os.Getenv("REDIS_HOST"), os.Getenv("REDIS_PORT"))
-	db := redis.NewClient(&redis.Options{Addr: addr, DB: 0})
+	db = redis.NewClient(&redis.Options{Addr: addr, DB: 0})
 	if err := db.Ping(context.Background()).Err(); err != nil {
 		log.Fatal(err)
 	}
