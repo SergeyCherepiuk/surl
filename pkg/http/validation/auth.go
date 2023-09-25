30ff0f61bef68da76e2436bc7d7aff0a4d734856
Sergey Cherepiuk <s.cherepiuk@gmail.com>
2023-09-25 21:51:57 +0200

    Improved password validation

--- pkg/http/validation/auth.go
+++ pkg/http/validation/auth.go
@@ -12,8 +12,14 @@ func ValidateUsername(username string) error {
 }
 
 func ValidatePassword(password string) error {
-	if !passwordRegexp.MatchString(password) {
-		return fmt.Errorf("invalid password")
+	if !passwordMin8CharactersRegexp.MatchString(password) {
+		return fmt.Errorf("password must be at least 8 characters long")
+	} else if !passwordAllowedCharactersRegexp.MatchString(password) {
+		return fmt.Errorf("only letters, numbers and special symbols are allowed")
+	} else if !passwordMin1SpecialCharacterRegexp.MatchString(password) {
+		return fmt.Errorf("password must contain at least 1 special character")
+	} else if !passwordMin3UppercaseCharactersRegexp.MatchString(password) {
+		return fmt.Errorf("password must contain at least 3 uppercase characters")
 	}
 	return nil
 }
--- pkg/http/validation/const.go
+++ pkg/http/validation/const.go
@@ -3,8 +3,11 @@ package validation
 import "regexp"
 
 var (
-	usernameRegexp = regexp.MustCompile(`^[[:alnum:]_]{3,30}$`)
-	passwordRegexp = regexp.MustCompile(`^[[:alnum:]@$!%*#?&]{8,}$`) // TODO: Improve validation to  require uppercase letters and digits
+	usernameRegexp                        = regexp.MustCompile(`^[[:alnum:]_]{3,30}$`)
+	passwordMin8CharactersRegexp          = regexp.MustCompile(`^.{8,}$`)
+	passwordAllowedCharactersRegexp       = regexp.MustCompile(`^[A-Za-z0-9!@#$%^&*]+$`)
+	passwordMin1SpecialCharacterRegexp    = regexp.MustCompile(`^(.*[!@#$%^&*]){1}.*$`)
+	passwordMin3UppercaseCharactersRegexp = regexp.MustCompile(`^(.*[A-Z]){3}.*$`)
 
 	// Source: https://stackoverflow.com/a/3809435
 	urlRegexp = regexp.MustCompile(`https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)`)
