87cf5bd09eedf49bb1162d8b28d117a314ab7905
Sergey Cherepiuk <s.cherepiuk@gmail.com>
2023-10-02 11:01:25 +0200

    SURL-16: Origin cache bug fix

--- pkg/database/redis/origin_getter.go
+++ pkg/database/redis/origin_getter.go
@@ -20,16 +20,16 @@ func NewOriginGetter(other domain.OriginGetter) *originGetter {
 func (og originGetter) Get(ctx context.Context, username, hash string) (string, time.Duration, error) {
 	key := fmt.Sprintf("%s/%s", username, hash)
 
-	origin, err := cacheDb.Get(ctx, key).Result()
-	if err == nil {
+	if origin, err := cacheDb.Get(ctx, key).Result(); err == nil {
 		ttl, _ := cacheDb.TTL(ctx, key).Result()
 		return origin, ttl, nil
 	}
 
 	origin, expiresIn, err := og.other.Get(context.Background(), username, hash)
-
 	ttl := internal.Min(5*time.Minute, expiresIn)
-	cacheDb.Set(ctx, key, origin, ttl)
+	if err == nil {
+		cacheDb.Set(ctx, key, origin, ttl)
+	}
 
 	return origin, ttl, err
 }
